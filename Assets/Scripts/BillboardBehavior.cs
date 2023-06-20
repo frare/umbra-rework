d28be5cb64e4c2cbeb22f5fd4f37eaa3e976cd53
Lucas Frare <31972242+frare@users.noreply.github.com>
2023-06-20 01:25:33 -0300

    Player não rotaciona mais em X

--- Assets/Scripts/BillboardBehavior.cs
+++ Assets/Scripts/BillboardBehavior.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class BillboardBehavior : MonoBehaviour
 {
+    [SerializeField] private bool ignoreX = false;
+
+
+
     private void LateUpdate()
     {
         transform.forward = this.transform.position - Camera.main.transform.position;
+        if (ignoreX) transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, transform.localEulerAngles.z);
     }
 }
